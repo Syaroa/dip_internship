bef26538ce37f57387b8e6bd8266ae7b041e51f3
unknown <tomo26.infinitree@gmail.com>
2020-10-15 13:59:42 +0900

    エラー処理を追加

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
+    #もしoutputが残っていれば削除
     if(os.path.exists('output.csv')):
         os.remove('output.csv')
     return render_template('submit.html')
@@ -24,13 +25,17 @@ def do_pred():
     
     idx = test_x['お仕事No.']
     # 勤務開始時刻、勤務終了時刻を計算
-    test_x['start_time'] = test_x['期間・時間\u3000勤務時間'].str[:2].str.strip(':').astype(int)
+    try:
+        test_x['start_time'] = test_x['期間・時間\u3000勤務時間'].str[:2].str.strip(':').astype(int)
+    except:
+        return render_template('error.html')
     test_x['end_time'] = test_x['期間・時間\u3000勤務時間'].str.extract('〜(\d{1,2}:\d{2})', expand=False)
     test_x['end_time'] = test_x['end_time'].str[:2]
     test_x['end_time'] = test_x['end_time'].str.strip(':').astype(int)
 
     # 予測に使用するカラムを抽出
-    test_x = test_x[['勤務地\u3000最寄駅1（駅名）', '職種コード', '週2・3日OK', '正社員登用あり',
+    try:
+        test_x = test_x[['勤務地\u3000最寄駅1（駅名）', '職種コード', '週2・3日OK', '正社員登用あり',
                                 '勤務地\u3000都道府県コード', '大量募集', '土日祝休み','駅から徒歩5分以内',
                                 '車通勤OK', '未経験OK', '16時前退社OK', '勤務地\u3000市区町村コード','Wordのスキルを活かす',
                                 '短時間勤務OK(1日4h以内)', '交通費別途支給', '英語力を活かす', '給与/交通費\u3000給与下限',
@@ -38,6 +43,9 @@ def do_pred():
                                 '大手企業', 'シフト勤務','経験者優遇','学校・公的機関（官公庁）', '英語力不要',
                                 '土日祝のみ勤務','期間・時間\u3000勤務期間','start_time','残業なし','オフィスが禁煙・分煙',
                                 'end_time', '給与/交通費\u3000交通費', '残業月20時間未満', '服装自由']]
+    except:
+        return render_template('error.html')
+
 
     # 勤務地最寄駅のカラムをラベルエンコーディングする
     le = pickle.load(open('labelencoder.pkl', 'rb'))
@@ -45,6 +53,7 @@ def do_pred():
         test_x['勤務地\u3000最寄駅1（駅名）'] = le.transform(test_x.loc[:,'勤務地\u3000最寄駅1（駅名）'].values)
     except:
         return render_template('error.html')
+
     #学習済みモデルをロード
     model = pickle.load(open('trained_model.pkl', 'rb'))
     predicted = model.predict(test_x)
